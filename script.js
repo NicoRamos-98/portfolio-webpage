eaa466025507d77a010eee88a6f03e83db9a10f0
Nico Ramos <nikko.eramos@me.com>
2025-06-27 19:27:18 -0700

    Update: Added mobile menu toggle and auto-close JS

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ if (now.getMonth() === 0 && now.getDate() >= 1) {
 } else {
   footerYear.textContent = 2025; 
 }
+
+
+const toggleButton = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+toggleButton.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('active');
+  });
+});
